76ca581a41c329ca75b37a65a372370e9074f553
comeevery-git <comeevery@gmail.com>
2021-02-21 19:04:48 +0900

    component life cycle...

--- src/App.js
+++ src/App.js
@@ -1,66 +1,57 @@
-import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
+import React from "react";
 import PropTypes from "prop-types";
 
-function Food({name, picture, insta}){
-  return (
-    <div>
-      <h1>I LIKE {name}!</h1>
-      <h4>TEST IMG {picture}</h4>
-      <h4>TEST INSTA {insta}</h4>
-    </div>
-  );
-}
-
-// npm i prop-types
-// https://ko.reactjs.org/docs/typechecking-with-proptypes.html
-Food.propTypes = {
-  // 얻고 싶은 props 타입에 대한 설명
-  id: PropTypes.number,
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  insta: PropTypes.string
-}
-
-const foodIList = [
-  {
-    id:1,
-    name:"kimchi",
-    image:"one",
-    insta:"vnbn"
-  },
-  {
-    id:2,
-    name:"ramen",
-    image:"two",
-    insta:"acxc"
-  },
-  {
-    id:3,
-    name: "samgiopsal",
-    image:"three",
-    insta:"qpwe"
-  },
-  {
-    id:4,
-    name: "chukumi",
-    image:"four",
-    insta:"gdjsp"
-  }
-];
-
+// function component
 // component : html을 반환하는 함수
-function App() {
-  return (
-    <div className="App">
-      {foodIList.map(dish => 
-        <Food
-         key={dish.id}
-         name={dish.name}
-         picture={dish.image}
-         insta={dish.insta} />
-      )}
+// function App() {
+//   return (
+//     <div className="App">
+//       {foodIList.map(dish => 
+//         <Food
+//          key={dish.id}
+//          name={dish.name}
+//          picture={dish.image}
+//          insta={dish.insta} />
+//       )}
+//     </div>
+//   );
+// }
+
+// class component = state
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("start");
+  }
+  state = {
+    // data, change
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count +1 }))
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count -1 }))
+  };
+  componentDidMount(){
+    console.log("component rended");
+  }
+  componentDidUpdate(){
+    console.log("component update");
+  }
+  componentWillUnmount(){
+    console.log("GoodBye, cruel world");
+  }
+  render(){
+    console.log("ing render");
+    return (
+    <div>
+      <h1>I AM CLASS {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
